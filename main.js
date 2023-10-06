960f6a2219c1095f70208a15d9a1e55adee8c020
ines2024 <ines.lahbib7@gmail.com>
2023-10-06 13:38:36 +0200

    save

---
+++ main.js
@@ -0,0 +1,53 @@
+var quantity=document.querySelectorAll("#quantity")
+console.log(quantity);
+var plus=document.querySelectorAll("#increment")
+console.log(increment);
+var moins=document.querySelectorAll("#decrement")
+console.log(decrement);
+var price=document.querySelectorAll("#price")
+console.log(price);
+var total=document.querySelector("#total-price")
+console.log(total-price);
+var heart=document.querySelectorAll(".material-symbols-outlined")
+
+
+function totalPrice (){
+var result=0
+for (let i = 0; i < price.length; i++) {
+    result=price[i].innerHTML*quantity[i].innerHTML+result
+    
+}
+return total.innerHTML=result
+}
+for (let i = 0; i< plus.length; i++) {
+    
+    plus[i].addEventListener("click",function add() {
+        quantity[i].innerHTML++
+        return totalPrice()
+    } );
+}
+for (let i = 0; i<moins.length; i++) {
+    
+    moins[i].addEventListener("click",function reduce() {
+        if (quantity[i].innerHTML >0) {
+            quantity[i].innerHTML--    
+        }
+        
+        return totalPrice()
+    } );
+}
+
+for (let i = 0; i< heart.length; i++) {
+
+    heart[i].addEventListener("click",function hearts() {
+        
+        if (heart[i].style.backgroundColor==="red"){
+
+          return  heart[i].style.backgroundColor="white"
+        }
+        else{
+           return heart[i].style.backgroundColor="red"
+        }
+    })
+    
+}
